c1476f96f3f1e7c30efbdbdb2e961084e88f3a14
LapadatGabriel99 <lapadatrobert123@gmail.com>
2024-04-07 23:35:54 +0300

    Fix error

--- Image.Api/Services/KeyVaultService.cs
+++ Image.Api/Services/KeyVaultService.cs
@@ -6,15 +6,17 @@ namespace Image.Api.Services
     public class KeyVaultService : ISecretService
     {
         private readonly SecretClient _secretClient;
+        private readonly IConfiguration _configuration;
 
-        public KeyVaultService(SecretClient secretClient)
+        public KeyVaultService(SecretClient secretClient, IConfiguration configuration)
         {
             _secretClient = secretClient;
+            _configuration = configuration;
         }
 
         public async Task<string> GetSecret(string secretName)
         {
-            var secret = await _secretClient.GetSecretAsync(secretName);
+            var secret = await _secretClient.GetSecretAsync(_configuration.GetValue<string>(secretName););
 
             if (secret.GetRawResponse().IsError)
             {
